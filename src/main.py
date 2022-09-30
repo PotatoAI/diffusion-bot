3329ba2d72678c72e3361abf5c6a05d40037a263
Max Gonzih <gonzih@gmail.com>
2022-09-30 10:13:53 -0700

    Improvements

--- src/main.py
+++ src/main.py
@@ -3,7 +3,8 @@ import os
 import uuid
 import coloredlogs
 import functools
-from logging import info
+import asyncio
+from logging import info, error, warn
 from diffusers import StableDiffusionPipeline, ModelMixin
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
@@ -15,36 +16,38 @@ pool_executor = ProcessPoolExecutor(1)
 
 
 def run_in_executor(f):
-
     @functools.wraps(f)
     def inner(*args, **kwargs):
         loop = asyncio.get_running_loop()
-        return loop.run_in_executor(pool_executor, lambda: f(*args, **kwargs))
+        return loop.run_in_executor(None, lambda: f(*args, **kwargs))
 
     return inner
 
 
 class NoCheck(ModelMixin):
     """Can be used in place of safety checker. Use responsibly and at your own risk."""
-
     def __init__(self):
         super().__init__()
         self.register_parameter(name='asdf',
                                 param=torch.nn.Parameter(torch.randn(3)))
 
     def forward(self, images=None, **kwargs):
+        warn('Skipping NSFW check')
         return images, [False]
 
 
 class DiffusionThing:
-
     def __init__(self):
+        info('Initializing model')
         pipe = StableDiffusionPipeline.from_pretrained(
             "CompVis/stable-diffusion-v1-4", use_auth_token=True)
         pipe.safety_checker = NoCheck()
         pipe.enable_attention_slicing()
-        # self.pipe = pipe.to("mps")
-        self.pipe = pipe.to("cuda")
+        device = 'cuda'
+        if torch.backends.mps.is_available():
+            device = 'mps'
+        info(f'Moving model to {device}')
+        self.pipe = pipe.to(device)
 
     @run_in_executor
     def run(self, prompt: str) -> str:
@@ -61,12 +64,16 @@ diffusion = DiffusionThing()
 
 
 async def generate(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    prompt = update.message.text.replace("/gen ", "")
-    fname = await diffusion.run(prompt)
-    with open(fname, 'rb') as photo:
-        await context.bot.send_photo(chat_id=update.effective_chat.id,
-                                     photo=photo,
-                                     caption=prompt)
+    prompt = update.message.text.replace("/gen", "").strip()
+    try:
+        fname = await diffusion.run(prompt)
+        with open(fname, 'rb') as photo:
+            await context.bot.send_photo(chat_id=update.effective_chat.id,
+                                         photo=photo,
+                                         caption=prompt)
+    except Exception as e:
+        error(e)
+        await update.message.reply_text(f"Exception: {e}")
     # os.remove(fname)
 
 
