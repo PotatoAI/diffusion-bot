3678711ed9bd26f852b2b1e271de80a2a6202522
Max Gonzih <gonzih@gmail.com>
2022-10-04 12:48:17 -0700

    So many edge cases

--- src/bot/args.py
+++ src/bot/args.py
@@ -19,8 +19,8 @@ class GenerateArgs(BaseModel):
         assert len(self.prompt) > 0, 'Prompt can\'t be empty'
         assert self.count > 0, 'Count has to be greater than 0'
         assert self.count <= 10, 'Count has to be lower than 10'
-        if self.count > 1:
-            assert self.seed == None, 'Cant specify seed if count is greater than 1'
+        if self.count > 1 and self.seedwalk == 0:
+            assert self.seed == None, 'Cant specify seed with count and without seedwalk'
 
     @classmethod
     def from_prompt(cls, prompt: str, command: str = '/gen'):
